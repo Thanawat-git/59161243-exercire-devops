cd485ac9a17caec95666e7013ee5b21eda6a5fb7
Thanawat-git <buysai123gw@gmail.com>
2020-11-06 16:22:35 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const items = [
+    {no: '1', name: 'Iphone 12 Pro Max'}
+]
+
+app.get('/', (req, res) => {
+    return res.send('Hello!')
+})
+
+// function getItem(){
+//     return items
+// }
+
+app.get('/item', (req, res) => {
+    return res.json(items)
+})
+
+app.listen(port, () => {
+    console.log(`Start Express application at ${port}`)
+})
+
+module.exports = app
---
+++ tests/test.js
@@ -0,0 +1,31 @@
+const assert = require('assert')
+const request = require('supertest')
+const app = require('../index.js')
+
+describe('Validate setting the test', () => {
+    it('should be ok', () => {
+        assert.ok(true)
+    })
+})
+
+describe('GET "/"', () => {
+    it('respond with Hello!'), (done) => {
+        request(app).get('/').expect('Hello!', done)
+    }
+})
+
+describe('GET "/item"', ()=> {
+    it('respond Items'), (done) => {
+        request(app).get('/item'.expect(done).toBeInstanceOf(Array))
+    }
+})
+
+// describe('GET "/item"', ()=> {
+//     it('respond Items'), (done) => {
+        
+//         let want = [
+//             {no: '1', name: 'Iphone 12 Pro Max'}
+//         ]
+//         request(app).get('/item'.expect(want, done)
+//         }
+// })
