26258c50417e965e465b7e0a99d2432ffc3bd7cb
Thanawat-git <buysai123gw@gmail.com>
2020-11-06 17:48:29 +0700

    test push 3

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const items = [
-    {no: '1', name: 'Iphone 12 Pro Max'}
+    {no: '1', name: 'Iphone 12 Pro Max'},
+    {no: '2', name: 'Iphone 12'},
+    {no: '3', name: 'Iphone 11'}
 ]
 
 app.get('/', (req, res) => {
-    return res.send('Hel')
+    return res.send('Hello!')
 })
 
 app.get('/item', (req, res) => {
