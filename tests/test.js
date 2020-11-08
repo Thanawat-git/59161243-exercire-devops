0fbde06d6e956699b58ee11d19132c8fe7322310
Thanawat-git <buysai123gw@gmail.com>
2020-11-08 10:29:09 +0700

    test push 4

--- tests/test.js
+++ tests/test.js
@@ -8,14 +8,20 @@ describe('Validate setting the test', () => {
     })
 })
 
-describe('GET "/"', () => {
-    it('respond with Hello!'), (done) => {
-        request(app).get('/').expect('Hello!', done)
-    }
-})
+// describe('GET "/item"', ()=> {
+//     it('respond Items'), (done) => {
+//         request(app).get('/item').expect(done).toBeInstanceOf(Array)
+//     }
+// })
 
-describe('GET "/item"', ()=> {
-    it('respond Items'), (done) => {
-        request(app).get('/item').expect(done).toBeInstanceOf(Array)
-    }
+describe('GET "/item"', ()=>{
+    it('Somthing', (done)=>{
+        let want = [
+            {no: '1', name: 'Iphone 12 Pro Max'},
+            {no: '2', name: 'Iphone 12'},
+            {no: '3', name: 'Iphone 11'}
+        ]
+
+        request(app).get('/item').expect(want, done)
+    })
 })
